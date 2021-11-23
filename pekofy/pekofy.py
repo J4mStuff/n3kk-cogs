843c6cbf440cd0aa10678f708c891fb5fe39ade9
Daniel Dudek <danieldudek94@gmail.com>
2021-11-23 01:56:35 +0000

    Fixing pekofy

--- pekofy/pekofy.py
+++ pekofy/pekofy.py
@@ -14,36 +14,37 @@ class Pekofy(commands.Cog):
     @commands.command()
     async def pekofy(self, ctx: commands.Context, text: str = None):
         """Pekofy the replied to message, previous message, or your own text."""
-        if not text:
-            if hasattr(ctx.message, "reference") and ctx.message.reference:
-                try:
-                    text = (
-                        await ctx.fetch_message(ctx.message.reference.message_id)
-                    ).content
-                except (discord.Forbidden, discord.NotFound, discord.HTTPException):
-                    pass
-            if not text:
-                text = (await ctx.channel.history(limit=2).flatten())[
-                    1
-                ].content or "I can't translate that!"
-        await ctx.send(self.pekofy(text))
+        #if not text:
+        #    if hasattr(ctx.message, "reference") and ctx.message.reference:
+        #        try:
+        #            text = (
+        #                await ctx.fetch_message(ctx.message.reference.message_id)
+        #            ).content
+        #        except (discord.Forbidden, discord.NotFound, discord.HTTPException):
+        #            pass
+        #    if not text:
+        #        text = (await ctx.channel.history(limit=2).flatten())[
+        #            1
+        #        ].content or "I can't translate that!"
+        #   text = self.pekofy(text)
+        await ctx.send(text+"123")
 
-    @commands.command()
-    async def unpekofy(self, ctx: commands.Context, *, text: str = None):
-        """Unpekofies the replied to message, previous message, or your own text."""
-        if not text:
-            if hasattr(ctx.message, "reference") and ctx.message.reference:
-                try:
-                    text = (
-                        await ctx.fetch_message(ctx.message.reference.message_id)
-                    ).content
-                except (discord.Forbidden, discord.NotFound, discord.HTTPException):
-                    pass
-            if not text:
-                text = (await ctx.channel.history(limit=2).flatten())[
-                    1
-                ].content or "I can't translate that!"
-        await ctx.send(self.unpekofy(text))
+    #@commands.command()
+    #async def unpekofy(self, ctx: commands.Context, *, text: str = None):
+    #    """Unpekofies the replied to message, previous message, or your own text."""
+    #    if not text:
+    #        if hasattr(ctx.message, "reference") and ctx.message.reference:
+    #            try:
+    #                text = (
+    #                    await ctx.fetch_message(ctx.message.reference.message_id)
+    #                ).content
+    #            except (discord.Forbidden, discord.NotFound, discord.HTTPException):
+    #                pass
+    #        if not text:
+    #            text = (await ctx.channel.history(limit=2).flatten())[
+    #                1
+    #            ].content or "I can't translate that!"
+    #    await ctx.send(self.unpekofy(text))
 
     def pekofy(self, text: str):
         """Pekofy a text message"""
