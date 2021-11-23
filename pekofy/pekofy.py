137dbbe5be2839ba2da979788ed75add0c7fd37c
Daniel Dudek <danieldudek94@gmail.com>
2021-11-23 02:26:40 +0000

    Fixing pekofy

--- pekofy/pekofy.py
+++ pekofy/pekofy.py
@@ -1,4 +1,5 @@
 from redbot.core import commands
+import discord
 
 class Pekofy(commands.Cog):
     #"""Pekofy messages Peko!"""
@@ -18,8 +19,20 @@ class Pekofy(commands.Cog):
     @commands.command()
     async def pekofy(self, ctx):
         """This does stuff!"""
-        # Your code will go here
-        await ctx.send("I can do stuff!") 
+        text = ""
+        if hasattr(ctx.message, "reference") and ctx.message.reference:
+            try:
+                text = (
+                    await ctx.fetch_message(ctx.message.reference.message_id)
+                ).content
+            except (discord.Forbidden, discord.NotFound, discord.HTTPException):
+                pass
+        if not text:
+            text = (await ctx.channel.history(limit=2).flatten())[
+                1
+            ].content or "I can't translate that!"
+        #await ctx.send(self.pekofy(text))
+        await ctx.send("123")
         
 #    @commands.command()
 #    async def pekofy(self, ctx):#, text: str = None):
