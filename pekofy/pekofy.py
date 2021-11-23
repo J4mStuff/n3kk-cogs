0be3c1d5a752a22dd7d39c04288524150a572afb
Daniel Dudek <danieldudek94@gmail.com>
2021-11-23 01:17:33 +0000

    Pekofy kinda works?

--- pekofy/pekofy.py
+++ pekofy/pekofy.py
@@ -1,13 +1,53 @@
 from redbot.core import commands
+import discord
+
 
 class Pekofy(commands.Cog):
-    """My custom cog"""
+    """Pekofy messages Peko!"""
+
+    __author__ = "N3kk"
+    __version__ = "0.1.0"
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def mycom(self, ctx):
-        """This does stuff!"""
-        # Your code will go here
-        await ctx.send("I can do stuff!")
+    async def pekofy(self, ctx: commands.Context, text: str = None):
+        """Pekofy the replied to message, previous message, or your own text."""
+        text = self.get_text(ctx, text)
+        await ctx.send(self.pekofy(text))
+
+    @commands.command()
+    async def unpekofy(self, ctx: commands.Context, *, text: str = None):
+        """Unpekofies the replied to message, previous message, or your own text."""
+        text = self.get_text(ctx, text)
+        await ctx.send(self.unpekofy(text))
+
+
+    async def get_text(self, ctx: commands.Context, text: str = None):
+        if not text:
+            if hasattr(ctx.message, "reference") and ctx.message.reference:
+                try:
+                    text = (
+                        await ctx.fetch_message(ctx.message.reference.message_id)
+                    ).content
+                except (discord.Forbidden, discord.NotFound, discord.HTTPException):
+                    pass
+            if not text:
+                text = (await ctx.channel.history(limit=2).flatten())[
+                    1
+                ].content or "I can't translate that!"
+        return text
+
+    def pekofy(self, text: str):
+        """Pekofy a text message"""
+
+        sentences = text.split(".")
+
+        for sentence in sentences:
+            sentence += ", Peko"
+
+        return text.join(".")
+
+    def unpekofy():
+        return "Not implemented, Peko!"
