fe0c2044a6edd6775fdd2ffb222f0b82feaccf43
Daniel Dudek <danieldudek94@gmail.com>
2021-11-24 01:41:21 +0000

    more pasta stuff

--- holopasta/holopasta.py
+++ holopasta/holopasta.py
@@ -20,6 +20,12 @@ class Holopasta(commands.Cog):
         """Posts Pekora pasta"""
         await ctx.send(self.peko_pasta)
 
+    @commands.command()
+    async def freeyapasta(self, ctx: commands.Context):
+        """Posts Freeya(r)'s pasta"""
+        await ctx.send(self.freeya_pasta)
+
+    """Credits to Bougainvillea#9131 and ɐʎǝǝɹℲ#1111 for writing this abomination"""
     korone_pasta = """
     I went to my bedroom and i see Korone lying on my bed half naked, she saw my boner got really hard and she starts to wag her tail like crazy, as i took my pant off she immediately jump to me and start sucking my pp until i came. After she was done with sucking me off I was so relieved I didn't even notice that she kept standing in this "begging dog" pose of hers. 
     Without much thinking I grabbed her by the arms, turned around and slid my dick right into her hairy hole. She started to whimper quietly, but her tail was running around like a helicopter all this time. After we were done, still happy as fuck she decided to jump on me and started licking my face like crazy. 
@@ -27,8 +33,14 @@ class Holopasta(commands.Cog):
     Then i forced my dog to lick my dick until i came, im still really hard so i just shove my dick inside it's ass and keep shouting "OHH KORONE YOUR PUSSY FEELS SO GOOD!" I keep fucking it until i came like 7 times and when i came for the last time i shouted "GET PREGNANT WITH MY BABY, KORONE!". I was really tired and i can't even stand up, my dog still licking my face so i hugged it and thinking that it's Korone that im hugging and i said " You'll always be my Korone"
     """
 
+    """I don't know who wrote it but fuck you"""
     peko_pasta = """
     So as a joke, I went to my friend's house wearing Pekora's wig and clothes. I could barely stop my laughter as he went as red as a tomato and looked at me from head to toe with a bit of drool in his mouth. The way he stared made me feel a bit funny too, but I decided to tease him more by taking off my clothes. He asked me, "Are you serious?" and I said "Yep peko."
 He went silent for what seemed like forever, so I asked him, "What's the matter peko?" He said he's confused, but then his b*ner got really hard, which made me take off his clothes. I expected him to scream, "Stop!" as I kissed him and stroked his c*ck, but he instead shouted "Oh God, Pekora!" which made me get a b*ner myself. Before I knew it, I was blowing him for the first time till he came.
 His s*men was so thick, it got stuck inside my throat no matter how hard I swallowed. He then said, "I want to f*ck you now!" and seeing that we've already gone that far and we were both n*ked, I obliged. A few hours later, the jerk went all pale and said to me "Why did we do that? Now I'm not f*cking straight." But he still looked so cute all confused like that, so I took pity on him and reassured while wiping his c*m off my face, "Let's just pretend I'm still Pekora."
 """
+
+    """Freeya small pp"""
+    freeya_pasta = """
+    Freeya's cummies are actually very good for you. They're not only protein rich they're also low calorie. This means you can get your gains without having to worry about bulking up and having to drop a bunch of weight. Not only that but Freeya drinks gallons of pineapple juice in order to make sure his cummies are up to your taste standards. So what are you waiting for try Freeya's cummies today.
+    """
